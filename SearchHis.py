c9b593bbe6890a083cdd1da9e851d3888b2fa706
peyman <peyman.sayyadi@gmail.com>
2018-04-09 22:31:02 -0400

    added csv file storage

--- SearchHis.py
+++ SearchHis.py
@@ -2,6 +2,7 @@
 import json
 import os
 from datetime import datetime
+import csv
 
 file_path = '/Users/MEHRA/Documents/Notebooks/Takeout 2/Searches/'
 file_list = os.listdir(file_path)
@@ -12,18 +13,26 @@ def json_to_list(file_path, file_name):
         gdic_zero = json.load(json_file)
         json_file.close()
         gdic_one = gdic_zero['event']
-        print(len(gdic_one))
+        # print(len(gdic_one))
         return gdic_one
 
-# combined = []
-# for file in file_list:
-#     combined += json_to_list(file_path, file)
-combined = json_to_list(file_path,file_list[0])
+# Combining all the files into one list
+combined = []
+for file in file_list:
+     combined += json_to_list(file_path, file)
 
-timestamp = combined[0]['query']['id'][0]['timestamp_usec'][:13]
-# web:  1475146082971
-# inja: 1372549834804729
-print(timestamp)
-# gooddate = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-# print(gooddate)
-print(combined[0]['query']['query_text'])
+# Cleaning up the list
+records = {}
+for i in range(1000): #len(combined)):
+    timestamp = combined[i]['query']['id'][0]['timestamp_usec'][:10]
+    gooddate = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+    term = combined[i]['query']['query_text']
+    records[gooddate] = term
+
+# Printing into csv file
+with open('SearchOutput.csv', 'w', newline='') as csvfile:
+    record_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar = '"',
+                                quoting=csv.QUOTE_MINIMAL)
+    for rtime, rterm in records.items():
+        record_writer.writerow([rtime,rterm])
