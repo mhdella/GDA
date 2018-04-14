1d27a84b6cef7d1c23ae35ec3377d998cf011fea
peyman <peyman.sayyadi@gmail.com>
2018-04-13 21:28:48 -0400

    csv commented out

--- SearchHis.py
+++ SearchHis.py
@@ -3,6 +3,8 @@ import json
 import os
 from datetime import datetime
 import csv
+import pandas
+from collections import Counter
 
 file_path = '/Users/MEHRA/Documents/Notebooks/Takeout 2/Searches/'
 file_list = os.listdir(file_path)
@@ -13,7 +15,6 @@ def json_to_list(file_path, file_name):
         gdic_zero = json.load(json_file)
         json_file.close()
         gdic_one = gdic_zero['event']
-        # print(len(gdic_one))
         return gdic_one
 
 # Combining all the files into one list
@@ -23,16 +24,27 @@ for file in file_list:
 
 # Cleaning up the list
 records = {}
-for i in range(1000): #len(combined)):
+ymonth = []
+binset = set()
+for i in range(len(combined)): #len(combined)):
     timestamp = combined[i]['query']['id'][0]['timestamp_usec'][:10]
     gooddate = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+    shortdate =datetime.fromtimestamp(int(timestamp)).strftime('%Y') #-%m')
+    ymonth.insert(i, shortdate)
+    binset.add(shortdate)
     term = combined[i]['query']['query_text']
     records[gooddate] = term
 
+bins = list(binset)
+
 # Printing into csv file
-with open('SearchOutput.csv', 'w', newline='') as csvfile:
-    record_writer = csv.writer(csvfile, delimiter=',',
-                                quotechar = '"',
-                                quoting=csv.QUOTE_MINIMAL)
-    for rtime, rterm in records.items():
-        record_writer.writerow([rtime,rterm])
+#with open('SearchOutput.csv', 'w', newline='') as csvfile:
+#    record_writer = csv.writer(csvfile, delimiter=',',
+#                                quotechar = '"',
+#                                quoting=csv.QUOTE_MINIMAL)
+#    for rtime, rterm in records.items():
+#        record_writer.writerow([rtime,rterm])
+
+letter_counts = Counter(ymonth)
+df = pandas.DataFrame.from_dict(letter_counts, orient='index')
+df.plot(kind='bar',title ='Number of Searches per Year',legend=False)
